147f66baaf6e551fea9577ca19fb31fe454ba48b
RomeuTalina <romeutalina@gmail.com>
2025-01-30 23:00:03 +0000

    Initial commit

---
+++ build/CMakeFiles/3DPlatformer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "3DPlatformer.exe"
+  "3DPlatformer.exe.manifest"
+  "3DPlatformer.pdb"
+  "CMakeFiles/3DPlatformer.dir/src/main.c.obj"
+  "CMakeFiles/3DPlatformer.dir/src/main.c.obj.d"
+  "lib3DPlatformer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3DPlatformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
